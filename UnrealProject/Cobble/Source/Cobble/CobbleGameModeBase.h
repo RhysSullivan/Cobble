db7593df7626aa6989f6d748b530071ec214d555
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2020-02-20 20:08:04 -0500

    Initial Project Setup

---
+++ UnrealProject/Cobble/Source/Cobble/CobbleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CobbleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COBBLE_API ACobbleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
