b4a0cf1d7a2d6250795d62778f7a9b08b0e87681
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2020-02-27 19:15:21 -0500

    Implemented a state machine for the character with relevant animations. State machine supports jumping, interacting, running, and idling.

--- UnrealProject/Cobble/Source/Cobble/CobblePaperCharacter.cpp
+++ UnrealProject/Cobble/Source/Cobble/CobblePaperCharacter.cpp
@@ -4,12 +4,15 @@
 #include "CobblePaperCharacter.h"
 #include "Components/InputComponent.h"
 #include "PaperFlipbookComponent.h"
-
-ACobblePaperCharacter::ACobblePaperCharacter() : Super()
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PaperFlipbook.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
+ACobblePaperCharacter::ACobblePaperCharacter()
 {	
 	PrimaryActorTick.bCanEverTick = true;
-	auto Flipbook = GetSprite();
-	Flipbook->CastShadow = true;
+	FlipbookComponent = GetSprite();
+	FlipbookComponent->CastShadow = true;
 }
 
 void ACobblePaperCharacter::BeginPlay()
@@ -17,24 +20,120 @@ void ACobblePaperCharacter::BeginPlay()
 	Super::BeginPlay();
 }
 
+void ACobblePaperCharacter::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{	
+	GetWorld()->GetTimerManager().ClearAllTimersForObject(this);
+}
+
 void ACobblePaperCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	RotateToMatchMovementDirection();
+	DoCobbleStateMachine();
+}
+
+void ACobblePaperCharacter::DoCobbleStateMachine()
+{
+	if (ShouldDoStateMachine()) // If we are not waiting on an animation to finish
+	{
+		if (GetCharacterMovement()->IsFalling())
+		{
+			if (GetVelocity().Z > 0)
+			{
+				FlipbookComponent->SetFlipbook(RisingFlipbook);
+			}
+			else
+			{
+				FlipbookComponent->SetFlipbook(FallingFlipbook);
+			}
+		}
+		else
+		{
+			if (GetVelocity().Size() > 0)
+			{
+				FlipbookComponent->SetFlipbook(RunFlipbook);
+			}
+			else
+			{
+				FlipbookComponent->SetFlipbook(IdleFlipbook);
+			}
+		}
+	}
+}
+
+bool ACobblePaperCharacter::ShouldDoStateMachine()
+{
+	// If we have active timer handles then an animation is playing.
+	return !JumpTimerHandle.IsValid() && !InteractTimerHandle.IsValid();
+}
+
+void ACobblePaperCharacter::RotateToMatchMovementDirection()
+{
+	if (GetVelocity().Size() > 0)
+	{
+		float FacingDirection = FVector::DotProduct(GetActorForwardVector(), GetVelocity().GetSafeNormal());
+		if (FacingDirection < -.5)
+			FlipbookComponent->SetRelativeRotation(FRotator(0, 180, 0));
+		else
+			FlipbookComponent->SetRelativeRotation(FRotator(0, 0, 0));
+	}
 }
 
 void ACobblePaperCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 	PlayerInputComponent->BindAxis("MoveHorizontal", this, &ACobblePaperCharacter::MoveHorizontal);
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACobblePaperCharacter::DoJump);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACobblePaperCharacter::PreJump);
+	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &ACobblePaperCharacter::Interact);
 }
 
 void ACobblePaperCharacter::MoveHorizontal(float Value)
 {
+	if (InteractTimerHandle.IsValid())
+		return;
 	AddMovementInput(GetActorForwardVector(), Value);
 }
 
+/*
+JUMPING
+*/
 void ACobblePaperCharacter::DoJump()
 {
 	Jump();
+	GetWorld()->GetTimerManager().ClearTimer(JumpTimerHandle);
 }
+
+void ACobblePaperCharacter::PreJump()
+{
+	if (!CanJump())
+		return;
+	FlipbookComponent->SetFlipbook(PreJumpFlipbook);
+	GetWorld()->GetTimerManager().SetTimer(JumpTimerHandle, this, &ACobblePaperCharacter::DoJump, FlipbookComponent->GetFlipbookLength(), false);
+}
+bool ACobblePaperCharacter::CanJump()
+{
+	return Super::CanJump() && !GetMovementComponent()->IsFalling() && !InteractTimerHandle.IsValid();
+}
+void ACobblePaperCharacter::Landed(const FHitResult& Hit)
+{
+	Super::Landed(Hit);
+	FlipbookComponent->SetFlipbook(LandingFlipbook);
+	GetWorld()->GetTimerManager().SetTimer(JumpTimerHandle, this, &ACobblePaperCharacter::PostLandedAnimation, FlipbookComponent->GetFlipbookLength(), false);
+}
+void ACobblePaperCharacter::PostLandedAnimation() { GetWorld()->GetTimerManager().ClearTimer(JumpTimerHandle); }
+
+/*
+INTERACTION
+*/
+void ACobblePaperCharacter::Interact()
+{
+	if (!CanInteract())
+		return;
+	FlipbookComponent->SetFlipbook(InteractFlipbook);
+	GetWorld()->GetTimerManager().SetTimer(InteractTimerHandle, this, &ACobblePaperCharacter::PostInteract, FlipbookComponent->GetFlipbookLength(), false);
+}
+bool ACobblePaperCharacter::CanInteract()
+{
+	return !GetMovementComponent()->IsFalling() && !JumpTimerHandle.IsValid();
+}
+void ACobblePaperCharacter::PostInteract(){GetWorld()->GetTimerManager().ClearTimer(InteractTimerHandle);}
--- UnrealProject/Cobble/Source/Cobble/CobblePaperCharacter.h
+++ UnrealProject/Cobble/Source/Cobble/CobblePaperCharacter.h
@@ -20,15 +20,67 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void RotateToMatchMovementDirection();
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+public:
+	UPROPERTY(EditAnywhere)
+	class UPaperFlipbook* RunFlipbook;
+	UPROPERTY(EditAnywhere)
+	class UPaperFlipbook* IdleFlipbook;
+	UPROPERTY(EditAnywhere)
+	class UPaperFlipbook* PreJumpFlipbook;
+	UPROPERTY(EditAnywhere)
+	class UPaperFlipbook* RisingFlipbook;
+	UPROPERTY(EditAnywhere)
+	class UPaperFlipbook* FallingFlipbook;
+	UPROPERTY(EditAnywhere)
+	class UPaperFlipbook* LandingFlipbook;
+	UPROPERTY(EditAnywhere)
+	class UPaperFlipbook* InteractFlipbook;
 private:
 	void MoveHorizontal(float Value);
+
+	void DoCobbleStateMachine();
+
+	bool ShouldDoStateMachine();
+
+
+	/*
+	PreJump - Play prejump anim and start timer. 
+	DoJump - Called after pre jump - clears timer, does jump
+	Written by Rhys Sullivan
+	*/
+	void PreJump();
+	bool CanJump();
+	UFUNCTION()
 	void DoJump();
+
+	/*
+	Landed - Plays the landing animation & starts a timer for when that ends. 
+	PostLandedAnimation - After the landing timer ends, clear the timer. 
+	Written by Rhys Sullivan
+	*/ 
+	void Landed(const FHitResult& Hit) override;
+	UFUNCTION()
+	void PostLandedAnimation();
+
+	/*
+	
+	Written by Rhys Sullivan
+	*/
+	void Interact();
+	bool CanInteract();
+	void PostInteract();
+private:
+	class UPaperFlipbookComponent* FlipbookComponent; // Reference to the flipbook pointer so we don't have to call GetSprite() over and over
+	FTimerHandle JumpTimerHandle; // Managers the timer for jumping related animations
+	FTimerHandle InteractTimerHandle;
 };
