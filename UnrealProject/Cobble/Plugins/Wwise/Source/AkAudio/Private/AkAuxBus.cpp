505e29c63e29984f733a0c29a5423341b269d1dc
Judah Avery <judahbavery@gmail.com>
2020-04-08 11:11:01 -0400

    Deleting Wwise plugin for Jeremiah

--- UnrealProject/Cobble/Plugins/Wwise/Source/AkAudio/Private/AkAuxBus.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2006-2019 Audiokinetic Inc. / All Rights Reservedn
-#include "AkAuxBus.h"
-
-UAkAssetData* UAkAuxBus::createAssetData(UObject* parent) const
-{
-	return NewObject<UAkAssetDataWithMedia>(parent);
-}
